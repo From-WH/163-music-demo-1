994cb2e101f93bce437a2de582e89bda9bf345b8
WangHang <from-wh@hotmail.com>
2018-05-22 23:57:28 +0800

    点击歌曲，取消新建歌曲高亮

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,11 +9,16 @@
       let $el = $(this.el)
       $el.html(this.template)
       let { songs } = data
-      let liList = songs.map((song) => $('<li></li>').text(song.name))
+      let liList = songs.map((song) => $('<li></li>').text(song.name).attr('data-song-id',song.id))
       $el.find('ul').empty()
       liList.map((domLi) => {
         $el.find('ul').append(domLi)
       })
+    },
+    activeItem(li){
+      let $li = $(li)
+      $li.addClass('active')
+        .siblings('.active').removeClass('active')
     },
     clearActive() {
       // $(this.el).find('.active'), removeClass('active')
@@ -38,6 +43,22 @@
       this.view = view
       this.model = model
       this.view.render(this.model.data)
+      this.bindEvent()
+      this.getAllSongs()
+    },
+    getAllSongs(){
+      return this.model.find().then(()=>{
+        this.view.render(this.model.data)
+      })
+    },
+    bindEvent(){
+      $(this.view.el).on('click','li',(e)=>{
+        this.view.activeItem(e.currentTarget)
+        let songId = e.currentTarget.getAttribute('data-song-id')
+        window.eventHub.emit('select',{id:songId})
+      })
+    },
+    bindEventHub(){
       window.eventHub.on('upload', () => {
         this.view.clearActive()
       })
@@ -45,13 +66,6 @@
         this.model.data.songs.push(songData)
         this.view.render(this.model.data)
       })
-      this.model.find().then(() => {
-        this.view.render(this.model.data)
-      })
-      this.model.find().then(()=>{
-        this.view.render(this.model.data)
-      })
-
     }
   }
   controller.init(view, model)

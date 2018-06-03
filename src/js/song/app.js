d28deae6f2c0dc1ef57a1f37dfbbde81c4c56fdb
WangHang <from-wh@hotmail.com>
2018-06-04 00:33:07 +0800

    歌词滚动（歌词时间第一个需为0否则有bug）

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -48,98 +48,98 @@
     showLyric(time) {
       let allP = this.$el.find('.lyric>.lines>p')
       let p
-      for(let i=0;i<allP.length;i++){
-        if(i===allP.length-1){
-          p = allP[0]
-        }else{
+      for (let i = 0; i < allP.length; i++) {
+        if (i === allP.length - 1) {
+          p = allP[i]
+        } else {
           let currentTime = allP.eq(i).attr('data-time')
-          let nextTime = allP.eq(i+1).attr('data-time')
-          if(currentTime <= time && time < nextTime){
+          let nextTime = allP.eq(i + 1).attr('data-time')
+          if (currentTime <= time && time < nextTime) {
             p = allP[i]
             break
           }
         }
       }
-        let pHeight = p.getBoundingClientRect().top
-        let linesHeight = this.$el.find('.lyric>.lines')[0].getBoundingClientRect().top
-        let height = pHeight - linesHeight
-        this.$el.find('.lyric>.lines').css({
-          transform: `translateY(${-(height - 25)}px)`
-        })
-        $(p).addClass('active').siblings('.active').removeClass('active')
-      },
-      play() {
-        this.$el.find('audio')[0].play()
-      },
-      pause() {
-        this.$el.find('audio')[0].pause()
-      }
+      let pHeight = p.getBoundingClientRect().top
+      let linesHeight = this.$el.find('.lyric>.lines')[0].getBoundingClientRect().top
+      let height = pHeight - linesHeight
+      this.$el.find('.lyric>.lines').css({
+        transform: `translateY(${-(height - 25)}px)`
+      })
+      $(p).addClass('active').siblings('.active').removeClass('active')
+    },
+    play() {
+      this.$el.find('audio')[0].play()
+    },
+    pause() {
+      this.$el.find('audio')[0].pause()
     }
-      let model = {
-      data: {
-        song: {
-          id: '',
-          name: '',
-          singer: '',
-          url: ''
-        },
-        status: 'paused'
-      },
-      get(id) {
-        var query = new AV.Query('Song');
-        return query.get(id).then((song) => {
-          Object.assign(this.data.song, {id: song.id}, song.attributes)
-          return song
-        })
-      }
-    }
-      let controller = {
-      init(view, model) {
-        this.view = view
-        this.view.init()
-        this.model = model
-        let id = this.getSongId()
-        this.model.get(id).then(() => {
-          this.view.render(this.model.data)
-        })
-        this.bindEvents()
-      },
-      bindEvents() {
-        $(this.view.el).on('click', '.icon-play', (e) => {
-          e.preventDefault()
-          this.model.data.status = 'playing'
-          this.view.render(this.model.data)
-          this.view.play()
-        })
-        $(this.view.el).on('click', '.icon-pause', (e) => {
-          e.preventDefault()
-          this.model.data.status = 'paused'
-          this.view.render(this.model.data)
-          this.view.pause()
-        })
-        window.eventHub.on('songEnd', () => {
-          this.model.data.status = 'paused'
-          this.view.render(this.model.data)
-        })
-      },
-      getSongId() {
-        let search = window.location.search     //获取查询参数
-        if (search.indexOf('?') === 0) {
-          search = search.substring(1)   //去掉第一个 从第二个开始显示
-        }
-        let array = search.split('&').filter((v => v)) //filter过滤空字符串   v=>v就是判断真，假
-        let id = ''
-        for (let i = 0; i < array.length; i++) {
-          let kv = array[i].split('=')
-          let key = kv[0]
-          let value = kv[1]
-          if (key === 'id') {
-            id = value
-            break
-          }
-        }
-        return id
-      }
-    }
-      controller.init(view, model)
   }
+  let model = {
+    data: {
+      song: {
+        id: '',
+        name: '',
+        singer: '',
+        url: ''
+      },
+      status: 'paused'
+    },
+    get(id) {
+      var query = new AV.Query('Song');
+      return query.get(id).then((song) => {
+        Object.assign(this.data.song, { id: song.id }, song.attributes)
+        return song
+      })
+    }
+  }
+  let controller = {
+    init(view, model) {
+      this.view = view
+      this.view.init()
+      this.model = model
+      let id = this.getSongId()
+      this.model.get(id).then(() => {
+        this.view.render(this.model.data)
+      })
+      this.bindEvents()
+    },
+    bindEvents() {
+      $(this.view.el).on('click', '.icon-play', (e) => {
+        e.preventDefault()
+        this.model.data.status = 'playing'
+        this.view.render(this.model.data)
+        this.view.play()
+      })
+      $(this.view.el).on('click', '.icon-pause', (e) => {
+        e.preventDefault()
+        this.model.data.status = 'paused'
+        this.view.render(this.model.data)
+        this.view.pause()
+      })
+      window.eventHub.on('songEnd', () => {
+        this.model.data.status = 'paused'
+        this.view.render(this.model.data)
+      })
+    },
+    getSongId() {
+      let search = window.location.search     //获取查询参数
+      if (search.indexOf('?') === 0) {
+        search = search.substring(1)   //去掉第一个 从第二个开始显示
+      }
+      let array = search.split('&').filter((v => v)) //filter过滤空字符串   v=>v就是判断真，假
+      let id = ''
+      for (let i = 0; i < array.length; i++) {
+        let kv = array[i].split('=')
+        let key = kv[0]
+        let value = kv[1]
+        if (key === 'id') {
+          id = value
+          break
+        }
+      }
+      return id
+    }
+  }
+  controller.init(view, model)
+}
